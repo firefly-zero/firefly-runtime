31266641509a963ba71efa4b072920341acdbd85
gram <git@orsinium.dev>
2024-04-02 12:27:40 +0200

    API to read input

--- src/input.rs
+++ src/input.rs
@@ -1,15 +1,34 @@
 use crate::state::State;
+use firefly_device::*;
 
 type C<'a> = wasmi::Caller<'a, State>;
 
-pub(crate) fn read_left(mut _caller: C) -> u64 {
-    todo!()
+pub(crate) fn read_left(mut caller: C) -> u32 {
+    let state = caller.data_mut();
+    let input = match state.device.read_input() {
+        Some(InputState {
+            left: Some(left), ..
+        }) => left,
+        _ => return 0,
+    };
+    let x = input.x as u32;
+    let y = input.y as u32;
+    x << 16 | y
 }
 
-pub(crate) fn read_right(mut _caller: C) -> u64 {
-    todo!()
+pub(crate) fn read_right(mut caller: C) -> u32 {
+    let state = caller.data_mut();
+    let input = match state.device.read_input() {
+        Some(InputState {
+            right: Some(right), ..
+        }) => right,
+        _ => return 0,
+    };
+    let x = input.x as u32;
+    let y = input.y as u32;
+    x << 16 | y
 }
 
-pub(crate) fn read_buttons(mut _caller: C) -> u64 {
+pub(crate) fn read_buttons(mut _caller: C) -> u32 {
     todo!()
 }
