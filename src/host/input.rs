f4c412bab21934d90451360946c37ee60074eff7
gram <git@orsinium.dev>
2024-07-07 10:38:20 +0200

    decouple input reading

--- src/host/input.rs
+++ src/host/input.rs
@@ -1,7 +1,5 @@
-use crate::{
-    error::HostError,
-    state::{NetHandler, State},
-};
+use crate::error::HostError;
+use crate::state::{NetHandler, State};
 use firefly_device::*;
 
 type C<'a> = wasmi::Caller<'a, State>;
@@ -9,25 +7,11 @@ type C<'a> = wasmi::Caller<'a, State>;
 pub(crate) fn read_pad(mut caller: C, player: u32) -> u32 {
     let state = caller.data_mut();
     state.called = "input.read_pad";
-    let pad: Pad = match state.net_handler.get_mut() {
-        NetHandler::FrameSyncer(syncer) => {
-            let Some(peer) = syncer.peers.get(player as usize) else {
-                state.log_error(HostError::UnknownPeer(player));
-                return 0xffff;
-            };
-            let Some(frame_state) = peer.states.get_current() else {
-                return 0xffff;
-            };
-            let input = frame_state.input;
-            match input.pad {
-                Some(p) => Pad { x: p.0, y: p.1 },
-                None => return 0xffff,
-            }
-        }
-        _ => match &state.input {
-            Some(InputState { pad: Some(pad), .. }) => pad.clone(),
-            _ => return 0xffff,
-        },
+    let Some(input) = get_input(state, player) else {
+        return 0xffff;
+    };
+    let Some(pad) = input.pad else {
+        return 0xffff;
     };
     let x = pad.x as u16 as u32;
     let y = pad.y as u16 as u32;
@@ -45,4 +29,24 @@ pub(crate) fn read_buttons(mut caller: C, _player: u32) -> u32 {
         res = (res << 1) | u32::from(button);
     }
     res
+}
+
+fn get_input(state: &mut State, player: u32) -> Option<InputState> {
+    let NetHandler::FrameSyncer(syncer) = state.net_handler.get_mut() else {
+        return state.input.clone();
+    };
+
+    let Some(peer) = syncer.peers.get(player as usize) else {
+        state.log_error(HostError::UnknownPeer(player));
+        return None;
+    };
+    let Some(frame_state) = peer.states.get_current() else {
+        return None;
+    };
+    let input = frame_state.input;
+    let input = InputState {
+        pad: input.pad.map(|p| Pad { x: p.0, y: p.1 }),
+        buttons: [false; 5],
+    };
+    Some(input)
 }
