633afe96a217d1c41fa7bd009501fe1c03014b7c
gram <git@orsinium.dev>
2026-02-06 19:23:14 +0100

    drop helper func

--- src/linking.rs
+++ src/linking.rs
@@ -4,6 +4,7 @@ use alloc::boxed::Box;
 use alloc::string::{String, ToString};
 use alloc::vec::Vec;
 use core::fmt;
+use wasmi::Func;
 
 /// A linking error that may be returned by [`populate_externals`].
 pub enum LinkingError {
@@ -84,23 +85,23 @@ fn select_graphics_external<'a>(
     fn_name: &str,
 ) -> Option<wasmi::Func> {
     let func = match fn_name {
-        "clear_screen" => host_func(ctx, graphics::clear_screen),
-        "set_color" => host_func(ctx, graphics::set_color),
-        "draw_point" => host_func(ctx, graphics::draw_point),
-        "draw_line" => host_func(ctx, graphics::draw_line),
-        "draw_rect" => host_func(ctx, graphics::draw_rect),
-        "draw_rounded_rect" => host_func(ctx, graphics::draw_rounded_rect),
-        "draw_circle" => host_func(ctx, graphics::draw_circle),
-        "draw_ellipse" => host_func(ctx, graphics::draw_ellipse),
-        "draw_triangle" => host_func(ctx, graphics::draw_triangle),
-        "draw_arc" => host_func(ctx, graphics::draw_arc),
-        "draw_sector" => host_func(ctx, graphics::draw_sector),
-        "draw_qr" => host_func(ctx, graphics::draw_qr),
-        "draw_text" => host_func(ctx, graphics::draw_text),
-        "draw_image" => host_func(ctx, graphics::draw_image),
-        "draw_sub_image" => host_func(ctx, graphics::draw_sub_image),
-        "set_canvas" => host_func(ctx, graphics::set_canvas),
-        "unset_canvas" => host_func(ctx, graphics::unset_canvas),
+        "clear_screen" => Func::wrap(ctx, graphics::clear_screen),
+        "set_color" => Func::wrap(ctx, graphics::set_color),
+        "draw_point" => Func::wrap(ctx, graphics::draw_point),
+        "draw_line" => Func::wrap(ctx, graphics::draw_line),
+        "draw_rect" => Func::wrap(ctx, graphics::draw_rect),
+        "draw_rounded_rect" => Func::wrap(ctx, graphics::draw_rounded_rect),
+        "draw_circle" => Func::wrap(ctx, graphics::draw_circle),
+        "draw_ellipse" => Func::wrap(ctx, graphics::draw_ellipse),
+        "draw_triangle" => Func::wrap(ctx, graphics::draw_triangle),
+        "draw_arc" => Func::wrap(ctx, graphics::draw_arc),
+        "draw_sector" => Func::wrap(ctx, graphics::draw_sector),
+        "draw_qr" => Func::wrap(ctx, graphics::draw_qr),
+        "draw_text" => Func::wrap(ctx, graphics::draw_text),
+        "draw_image" => Func::wrap(ctx, graphics::draw_image),
+        "draw_sub_image" => Func::wrap(ctx, graphics::draw_sub_image),
+        "set_canvas" => Func::wrap(ctx, graphics::set_canvas),
+        "unset_canvas" => Func::wrap(ctx, graphics::unset_canvas),
         _ => return None,
     };
     Some(func)
@@ -111,35 +112,35 @@ fn select_audio_external<'a>(
     fn_name: &str,
 ) -> Option<wasmi::Func> {
     let func = match fn_name {
-        "reset" => host_func(ctx, audio::reset),
-        "reset_all" => host_func(ctx, audio::reset_all),
-        "clear" => host_func(ctx, audio::clear),
-        "add_empty" => host_func(ctx, audio::add_empty),
-        "add_file" => host_func(ctx, audio::add_file),
-        "add_mix" => host_func(ctx, audio::add_mix),
-        "add_all_for_one" => host_func(ctx, audio::add_all_for_one),
-        "add_gain" => host_func(ctx, audio::add_gain),
-        "add_loop" => host_func(ctx, audio::add_loop),
-        "add_concat" => host_func(ctx, audio::add_concat),
-        "add_pan" => host_func(ctx, audio::add_pan),
-        "add_mute" => host_func(ctx, audio::add_mute),
-        "add_pause" => host_func(ctx, audio::add_pause),
-        "add_track_position" => host_func(ctx, audio::add_track_position),
-        "add_low_pass" => host_func(ctx, audio::add_low_pass),
-        "add_high_pass" => host_func(ctx, audio::add_high_pass),
-        "add_take_left" => host_func(ctx, audio::add_take_left),
-        "add_take_right" => host_func(ctx, audio::add_take_right),
-        "add_swap" => host_func(ctx, audio::add_swap),
-        "add_clip" => host_func(ctx, audio::add_clip),
-        "add_noise" => host_func(ctx, audio::add_noise),
-        "add_sine" => host_func(ctx, audio::add_sine),
-        "add_square" => host_func(ctx, audio::add_square),
-        "add_sawtooth" => host_func(ctx, audio::add_sawtooth),
-        "add_triangle" => host_func(ctx, audio::add_triangle),
-        "add_zero" => host_func(ctx, audio::add_zero),
-        "mod_linear" => host_func(ctx, audio::mod_linear),
-        "mod_hold" => host_func(ctx, audio::mod_hold),
-        "mod_sine" => host_func(ctx, audio::mod_sine),
+        "reset" => Func::wrap(ctx, audio::reset),
+        "reset_all" => Func::wrap(ctx, audio::reset_all),
+        "clear" => Func::wrap(ctx, audio::clear),
+        "add_empty" => Func::wrap(ctx, audio::add_empty),
+        "add_file" => Func::wrap(ctx, audio::add_file),
+        "add_mix" => Func::wrap(ctx, audio::add_mix),
+        "add_all_for_one" => Func::wrap(ctx, audio::add_all_for_one),
+        "add_gain" => Func::wrap(ctx, audio::add_gain),
+        "add_loop" => Func::wrap(ctx, audio::add_loop),
+        "add_concat" => Func::wrap(ctx, audio::add_concat),
+        "add_pan" => Func::wrap(ctx, audio::add_pan),
+        "add_mute" => Func::wrap(ctx, audio::add_mute),
+        "add_pause" => Func::wrap(ctx, audio::add_pause),
+        "add_track_position" => Func::wrap(ctx, audio::add_track_position),
+        "add_low_pass" => Func::wrap(ctx, audio::add_low_pass),
+        "add_high_pass" => Func::wrap(ctx, audio::add_high_pass),
+        "add_take_left" => Func::wrap(ctx, audio::add_take_left),
+        "add_take_right" => Func::wrap(ctx, audio::add_take_right),
+        "add_swap" => Func::wrap(ctx, audio::add_swap),
+        "add_clip" => Func::wrap(ctx, audio::add_clip),
+        "add_noise" => Func::wrap(ctx, audio::add_noise),
+        "add_sine" => Func::wrap(ctx, audio::add_sine),
+        "add_square" => Func::wrap(ctx, audio::add_square),
+        "add_sawtooth" => Func::wrap(ctx, audio::add_sawtooth),
+        "add_triangle" => Func::wrap(ctx, audio::add_triangle),
+        "add_zero" => Func::wrap(ctx, audio::add_zero),
+        "mod_linear" => Func::wrap(ctx, audio::mod_linear),
+        "mod_hold" => Func::wrap(ctx, audio::mod_hold),
+        "mod_sine" => Func::wrap(ctx, audio::mod_sine),
         _ => return None,
     };
     Some(func)
@@ -150,8 +151,8 @@ fn select_input_external<'a>(
     fn_name: &str,
 ) -> Option<wasmi::Func> {
     let func = match fn_name {
-        "read_pad" => host_func(ctx, input::read_pad),
-        "read_buttons" => host_func(ctx, input::read_buttons),
+        "read_pad" => Func::wrap(ctx, input::read_pad),
+        "read_buttons" => Func::wrap(ctx, input::read_buttons),
         _ => return None,
     };
     Some(func)
@@ -162,9 +163,9 @@ fn select_menu_external<'a>(
     fn_name: &str,
 ) -> Option<wasmi::Func> {
     let func = match fn_name {
-        "add_menu_item" => host_func(ctx, menu::add_menu_item),
-        "remove_menu_item" => host_func(ctx, menu::remove_menu_item),
-        "open_menu" => host_func(ctx, menu::open_menu),
+        "add_menu_item" => Func::wrap(ctx, menu::add_menu_item),
+        "remove_menu_item" => Func::wrap(ctx, menu::remove_menu_item),
+        "open_menu" => Func::wrap(ctx, menu::open_menu),
         _ => return None,
     };
     Some(func)
@@ -175,12 +176,12 @@ fn select_fs_external<'a>(
     fn_name: &str,
 ) -> Option<wasmi::Func> {
     let func = match fn_name {
-        "get_rom_file_size" => host_func(ctx, fs::get_rom_file_size),
-        "load_rom_file" => host_func(ctx, fs::load_rom_file),
-        "get_file_size" => host_func(ctx, fs::get_file_size),
-        "load_file" => host_func(ctx, fs::load_file),
-        "dump_file" => host_func(ctx, fs::dump_file),
-        "remove_file" => host_func(ctx, fs::remove_file),
+        "get_rom_file_size" => Func::wrap(ctx, fs::get_rom_file_size),
+        "load_rom_file" => Func::wrap(ctx, fs::load_rom_file),
+        "get_file_size" => Func::wrap(ctx, fs::get_file_size),
+        "load_file" => Func::wrap(ctx, fs::load_file),
+        "dump_file" => Func::wrap(ctx, fs::dump_file),
+        "remove_file" => Func::wrap(ctx, fs::remove_file),
         _ => return None,
     };
     Some(func)
@@ -191,10 +192,10 @@ fn select_net_external<'a>(
     fn_name: &str,
 ) -> Option<wasmi::Func> {
     let func = match fn_name {
-        "get_me" => host_func(ctx, net::get_me),
-        "get_peers" => host_func(ctx, net::get_peers),
-        "save_stash" => host_func(ctx, net::save_stash),
-        "load_stash" => host_func(ctx, net::load_stash),
+        "get_me" => Func::wrap(ctx, net::get_me),
+        "get_peers" => Func::wrap(ctx, net::get_peers),
+        "save_stash" => Func::wrap(ctx, net::save_stash),
+        "load_stash" => Func::wrap(ctx, net::load_stash),
         _ => return None,
     };
     Some(func)
@@ -205,8 +206,8 @@ fn select_stats_external<'a>(
     fn_name: &str,
 ) -> Option<wasmi::Func> {
     let func = match fn_name {
-        "add_progress" => host_func(ctx, stats::add_progress),
-        "add_score" => host_func(ctx, stats::add_score),
+        "add_progress" => Func::wrap(ctx, stats::add_progress),
+        "add_score" => Func::wrap(ctx, stats::add_score),
         _ => return None,
     };
     Some(func)
@@ -217,14 +218,14 @@ fn select_misc_external<'a>(
     fn_name: &str,
 ) -> Option<wasmi::Func> {
     let func = match fn_name {
-        "log_debug" => host_func(ctx, misc::log_debug),
-        "log_error" => host_func(ctx, misc::log_error),
-        "set_seed" => host_func(ctx, misc::set_seed),
-        "get_random" => host_func(ctx, misc::get_random),
-        "get_name" => host_func(ctx, misc::get_name),
-        "restart" => host_func(ctx, misc::restart),
-        "set_conn_status" => host_func(ctx, misc::set_conn_status),
-        "quit" => host_func(ctx, misc::quit),
+        "log_debug" => Func::wrap(ctx, misc::log_debug),
+        "log_error" => Func::wrap(ctx, misc::log_error),
+        "set_seed" => Func::wrap(ctx, misc::set_seed),
+        "get_random" => Func::wrap(ctx, misc::get_random),
+        "get_name" => Func::wrap(ctx, misc::get_name),
+        "restart" => Func::wrap(ctx, misc::restart),
+        "set_conn_status" => Func::wrap(ctx, misc::set_conn_status),
+        "quit" => Func::wrap(ctx, misc::quit),
         _ => return None,
     };
     Some(func)
@@ -235,13 +236,13 @@ fn select_sudo_external<'a>(
     fn_name: &str,
 ) -> Option<wasmi::Func> {
     let func = match fn_name {
-        "list_dirs" => host_func(ctx, sudo::list_dirs),
-        "list_dirs_buf_size" => host_func(ctx, sudo::list_dirs_buf_size),
-        "list_files" => host_func(ctx, sudo::list_files),
-        "list_files_buf_size" => host_func(ctx, sudo::list_files_buf_size),
-        "get_file_size" => host_func(ctx, sudo::get_file_size),
-        "load_file" => host_func(ctx, sudo::load_file),
-        "run_app" => host_func(ctx, sudo::run_app),
+        "list_dirs" => Func::wrap(ctx, sudo::list_dirs),
+        "list_dirs_buf_size" => Func::wrap(ctx, sudo::list_dirs_buf_size),
+        "list_files" => Func::wrap(ctx, sudo::list_files),
+        "list_files_buf_size" => Func::wrap(ctx, sudo::list_files_buf_size),
+        "get_file_size" => Func::wrap(ctx, sudo::get_file_size),
+        "load_file" => Func::wrap(ctx, sudo::load_file),
+        "run_app" => Func::wrap(ctx, sudo::run_app),
         _ => return None,
     };
     Some(func)
@@ -252,14 +253,14 @@ fn select_wasip1_external<'a>(
     fn_name: &str,
 ) -> Option<wasmi::Func> {
     let func = match fn_name {
-        "environ_get" => host_func(ctx, wasip1::environ_get),
-        "environ_sizes_get" => host_func(ctx, wasip1::environ_sizes_get),
-        "clock_time_get" => host_func(ctx, wasip1::clock_time_get),
-        "fd_close" => host_func(ctx, wasip1::fd_close),
-        "fd_read" => host_func(ctx, wasip1::fd_read),
-        "fd_seek" => host_func(ctx, wasip1::fd_seek),
-        "fd_write" => host_func(ctx, wasip1::fd_write),
-        "proc_exit" => host_func(ctx, wasip1::proc_exit),
+        "environ_get" => Func::wrap(ctx, wasip1::environ_get),
+        "environ_sizes_get" => Func::wrap(ctx, wasip1::environ_sizes_get),
+        "clock_time_get" => Func::wrap(ctx, wasip1::clock_time_get),
+        "fd_close" => Func::wrap(ctx, wasip1::fd_close),
+        "fd_read" => Func::wrap(ctx, wasip1::fd_read),
+        "fd_seek" => Func::wrap(ctx, wasip1::fd_seek),
+        "fd_write" => Func::wrap(ctx, wasip1::fd_write),
+        "proc_exit" => Func::wrap(ctx, wasip1::proc_exit),
         _ => return None,
     };
     Some(func)
@@ -270,23 +271,23 @@ fn select_graphics_external_alias<'a>(
     fn_name: &str,
 ) -> Option<wasmi::Func> {
     let func = match fn_name {
-        "a" => host_func(ctx, graphics::draw_arc),
-        "c" => host_func(ctx, graphics::draw_circle),
-        "ca" => host_func(ctx, graphics::set_canvas),
-        "cr" => host_func(ctx, graphics::unset_canvas),
-        "cs" => host_func(ctx, graphics::clear_screen),
-        "e" => host_func(ctx, graphics::draw_ellipse),
-        "i" => host_func(ctx, graphics::draw_image),
-        "l" => host_func(ctx, graphics::draw_line),
-        "p" => host_func(ctx, graphics::draw_point),
-        "r" => host_func(ctx, graphics::draw_rect),
-        "rr" => host_func(ctx, graphics::draw_rounded_rect),
-        "s" => host_func(ctx, graphics::draw_sector),
-        "sc" => host_func(ctx, graphics::set_color),
-        "si" => host_func(ctx, graphics::draw_sub_image),
-        "t" => host_func(ctx, graphics::draw_triangle),
-        "x" => host_func(ctx, graphics::draw_text),
-        "q" => host_func(ctx, graphics::draw_qr),
+        "a" => Func::wrap(ctx, graphics::draw_arc),
+        "c" => Func::wrap(ctx, graphics::draw_circle),
+        "ca" => Func::wrap(ctx, graphics::set_canvas),
+        "cr" => Func::wrap(ctx, graphics::unset_canvas),
+        "cs" => Func::wrap(ctx, graphics::clear_screen),
+        "e" => Func::wrap(ctx, graphics::draw_ellipse),
+        "i" => Func::wrap(ctx, graphics::draw_image),
+        "l" => Func::wrap(ctx, graphics::draw_line),
+        "p" => Func::wrap(ctx, graphics::draw_point),
+        "r" => Func::wrap(ctx, graphics::draw_rect),
+        "rr" => Func::wrap(ctx, graphics::draw_rounded_rect),
+        "s" => Func::wrap(ctx, graphics::draw_sector),
+        "sc" => Func::wrap(ctx, graphics::set_color),
+        "si" => Func::wrap(ctx, graphics::draw_sub_image),
+        "t" => Func::wrap(ctx, graphics::draw_triangle),
+        "x" => Func::wrap(ctx, graphics::draw_text),
+        "q" => Func::wrap(ctx, graphics::draw_qr),
         _ => return None,
     };
     Some(func)
@@ -297,8 +298,8 @@ fn select_input_external_alias<'a>(
     fn_name: &str,
 ) -> Option<wasmi::Func> {
     let func = match fn_name {
-        "p" => host_func(ctx, input::read_pad),
-        "b" => host_func(ctx, input::read_buttons),
+        "p" => Func::wrap(ctx, input::read_pad),
+        "b" => Func::wrap(ctx, input::read_buttons),
         _ => return None,
     };
     Some(func)
@@ -309,10 +310,10 @@ fn select_net_external_alias<'a>(
     fn_name: &str,
 ) -> Option<wasmi::Func> {
     let func = match fn_name {
-        "l" => host_func(ctx, net::load_stash),
-        "m" => host_func(ctx, net::get_me),
-        "p" => host_func(ctx, net::get_peers),
-        "s" => host_func(ctx, net::save_stash),
+        "l" => Func::wrap(ctx, net::load_stash),
+        "m" => Func::wrap(ctx, net::get_me),
+        "p" => Func::wrap(ctx, net::get_peers),
+        "s" => Func::wrap(ctx, net::save_stash),
         _ => return None,
     };
     Some(func)
@@ -323,8 +324,8 @@ fn select_stats_external_alias<'a>(
     fn_name: &str,
 ) -> Option<wasmi::Func> {
     let func = match fn_name {
-        "p" => host_func(ctx, stats::add_progress),
-        "s" => host_func(ctx, stats::add_score),
+        "p" => Func::wrap(ctx, stats::add_progress),
+        "s" => Func::wrap(ctx, stats::add_score),
         _ => return None,
     };
     Some(func)
@@ -335,22 +336,13 @@ fn select_misc_external_alias<'a>(
     fn_name: &str,
 ) -> Option<wasmi::Func> {
     let func = match fn_name {
-        "d" => host_func(ctx, misc::log_debug),
-        "e" => host_func(ctx, misc::log_error),
-        "n" => host_func(ctx, misc::get_name),
-        "q" => host_func(ctx, misc::quit),
-        "r" => host_func(ctx, misc::get_random),
-        "s" => host_func(ctx, misc::set_seed),
+        "d" => Func::wrap(ctx, misc::log_debug),
+        "e" => Func::wrap(ctx, misc::log_error),
+        "n" => Func::wrap(ctx, misc::get_name),
+        "q" => Func::wrap(ctx, misc::quit),
+        "r" => Func::wrap(ctx, misc::get_random),
+        "s" => Func::wrap(ctx, misc::set_seed),
         _ => return None,
     };
     Some(func)
-}
-
-/// Utility function to wrap host functions without Wasmi imports.
-#[inline]
-fn host_func<T, P, R>(
-    ctx: impl wasmi::AsContextMut<Data = T>,
-    func: impl wasmi::IntoFunc<T, P, R>,
-) -> wasmi::Func {
-    wasmi::Func::wrap(ctx, func)
 }
